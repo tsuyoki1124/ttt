761b096026d4d58358a4854c73ad1c046d60ee8c
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-27 00:43:03 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,23 @@
 class MessagesController < ApplicationController
   def index
-    @country = Country.all
+    @name = Country.all
+    @message = Message.new
+    @country = Country.find(params[:country_id])
+  end
+
+  def create
+    @country = Country.find(params[:country_id])
+    @message = @country.messages.new(message_params)
+    if @message.save
+      redirect_to country_messages_path(@country)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :text).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "countries#index"
   resources :users, only: [:edit, :update]
-  resources :countries, only: [:new, :create]
+  resources :countries, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 
 end
