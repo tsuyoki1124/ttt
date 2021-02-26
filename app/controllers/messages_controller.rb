75126124627e55571acae4d172fe3c46a634ea5c
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-27 00:55:36 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
     @name = Country.all
     @message = Message.new
     @country = Country.find(params[:country_id])
+    @messages = @country.messages.includes(:user)
   end
 
   def create
@@ -11,6 +12,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to country_messages_path(@country)
     else
+      @messages = @country.messages.includes(:user)
       render :index
     end
   end
