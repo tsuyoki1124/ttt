caf56625abcef3af98681d8df4645ed958268067
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-26 23:45:02 +0900

    Revert "z"
    This reverts commit 558c21cbfea48f41df7c47bd34799e04a660a6e8.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "countries#index"
+  root to: "messages#index"
   resources :users, only: [:edit, :update]
-  resources :countries, only: [:new, :create]
-
 end
