f73736a4a1b5faa99787449bb06e6b493caa7dae
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-26 22:55:57 +0900

    チャットルーム作成機能の実装

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,4 +2,21 @@ class CountriesController < ApplicationController
   def new
     @country = Country.new
   end
+
+  def create
+    @country = Country.new(country_params)
+    if @country.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def country_params
+    params.require(:country).permit(:name)
+  end
+  
+
 end
