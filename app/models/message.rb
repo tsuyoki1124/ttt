cd734bd27f9b22bc2143a8b2ca42df0af2708e52
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-27 15:43:34 +0900

    validates

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,8 @@ class Message < ApplicationRecord
   belongs_to :country
   belongs_to :user
   has_one_attached :image
-  
+
   validates :title, presence: true
   validates :text, presence: true
-
+  validates :image, presence: true
 end
