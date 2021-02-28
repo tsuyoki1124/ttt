4a12029f29a33cff9d27e873156c7894f2904ba0
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-28 12:30:48 +0900

    countries tableにuser_id カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_020256) do
+ActiveRecord::Schema.define(version: 2021_02_28_032859) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2021_02_28_020256) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_countries_on_user_id"
   end
 
   create_table "country_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -81,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_02_28_020256) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "countries", "users"
   add_foreign_key "country_users", "countries"
   add_foreign_key "country_users", "users"
   add_foreign_key "messages", "countries"
